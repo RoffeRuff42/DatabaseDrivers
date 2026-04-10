919881c61ef5b20ebd056a0472034c48d2c93d88
Lisa Ebbhagen <lisa.ebbhagen@gmail.com>
2026-04-10 09:13:52 +0200

    Work in progress: IHttpClientFactory

--- DatabaseDrivers/Program.cs
+++ DatabaseDrivers/Program.cs
@@ -1,5 +1,6 @@
 
 using Scalar.AspNetCore;
+using TodoApi.Services;
 namespace DatabaseDrivers
 {
     public class Program
@@ -13,6 +14,41 @@ namespace DatabaseDrivers
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddHttpClient<ITodoService, TodoService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7194");
+                client.Timeout = TimeSpan.FromMinutes(2);
+            })
+            .AddStandardResilienceHandler(options =>
+            {
+                options.TotalRequestTimeout.Timeout = TimeSpan.FromMinutes(3);
+                options.AttemptTimeout.Timeout = TimeSpan.FromMinutes(2);
+
+                options.CircuitBreaker.SamplingDuration = TimeSpan.FromMinutes(4);
+
+                options.Retry.MaxRetryAttempts = 3;
+                options.Retry.Delay = TimeSpan.FromSeconds(5);
+                options.Retry.BackoffType = Polly.DelayBackoffType.Exponential;
+            });
+
+            //change to right url and client name after decision
+            builder.Services.AddHttpClient("ExternalApi", client =>
+            {
+                client.BaseAddress = new Uri("https://external-service.com");
+                client.Timeout = TimeSpan.FromMinutes(2);
+            })
+            .AddStandardResilienceHandler(options =>
+            {
+                options.TotalRequestTimeout.Timeout = TimeSpan.FromMinutes(3);
+                options.AttemptTimeout.Timeout = TimeSpan.FromMinutes(2);
+
+                options.CircuitBreaker.SamplingDuration = TimeSpan.FromMinutes(4);
+
+                options.Retry.MaxRetryAttempts = 3;
+                options.Retry.Delay = TimeSpan.FromSeconds(5);
+                options.Retry.BackoffType = Polly.DelayBackoffType.Exponential;
+            });
 
             var app = builder.Build();
 

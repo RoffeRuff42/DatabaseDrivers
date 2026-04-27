a6d4cf3e80f3cc286872cb930607645d18414e5e
Lisa Ebbhagen <lisa.ebbhagen@gmail.com>
2026-04-27 15:33:57 +0200

    Scalar UI via swashbuckle for UserAPI as well

--- ServiceB/Program.cs
+++ ServiceB/Program.cs
@@ -1,9 +1,10 @@
-using Microsoft.AspNetCore.RateLimiting;
-using Scalar.AspNetCore;
-using UserApi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
 using System.Text;
+using UserApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +26,39 @@ var jwtAudience = builder.Configuration["Jwt:Audience"];
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var currentDirectory = AppContext.BaseDirectory;
+    var xmlFiles = Directory.GetFiles(currentDirectory, "*.xml");
+    foreach (var xmlFile in xmlFiles)
+    {
+        options.IncludeXmlComments(xmlFile);
+    }
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        In = ParameterLocation.Header,
+        BearerFormat = "JWT"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 //Ratelimiting
 builder.Services.AddRateLimiter(options =>
@@ -68,7 +102,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapScalarApiReference();
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "openapi/{documentName}.json";
+    });
+    app.MapScalarApiReference(options =>
+    {
+        options.WithOpenApiRoutePattern("/openapi/v1.json");
+    });
+    app.UseCors("DevelopmentPolicy");
+}
+else
+{
+    app.UseCors("ProductionPolicy");
 }
 
 app.UseHttpsRedirection();

8ddf82b942fed14ad2af37ccdef2312532e61acc
Grahnnen <robin.grahn@live.se>
2026-06-01 12:34:30 +0200

    Added OpenAi and Azure KeyVault Integration.

---
+++ DatabaseDrivers/Clients/IAiClient.cs
@@ -0,0 +1,10 @@
+﻿using TodoApi.DTOs;
+
+namespace TodoApi.Clients;
+
+public interface IAiClient
+{
+    Task<AiDescriptionResponseDto> GenerateDescriptionAsync(
+        string title,
+        CancellationToken cancellationToken = default);
+}
---
+++ DatabaseDrivers/DTOs/AiDescriptionRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.DTOs;
+
+public sealed class AiDescriptionRequestDto
+{
+    [Required]
+    [StringLength(100, MinimumLength = 1)]
+    public required string Title { get; init; }
+}
